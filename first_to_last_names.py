021f51098d56dfb1b6672988e4333194f8ec3a3f
patrickian <patrick.concepcion33@gmail.com>
2015-12-08 02:17:23 -0500

    Transform the old process into list-comprehension.

--- first_to_last_names.py
+++ first_to_last_names.py
@@ -1,8 +1,11 @@
 first_names = ['Shan', 'Dao Ming', 'Hua Ze', 'Xi', 'Mei']
 last_names = ['Cai', 'Si', 'Lei', 'Men', 'Zuo']
-ctr = 0
-fullname = []
-for x in last_names:
-	fullname.append(first_names[ctr] + " " + x)
-	ctr+=1
-print fullname
+#ctr = 0
+#fullname = []
+#for x in last_names:
+#	fullname.append(first_names[ctr] + " " + x)
+#	ctr+=1
+#print fullname
+
+fullnames = [('{} {}'.format(x, y)) for x,y in zip(first_names,last_names)]
+print fullnames
