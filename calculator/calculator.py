d2710dec099741d260ae68e25d11d497e987e5bd
patrickian <patrick.concepcion33@gmail.com>
2015-12-11 01:45:02 -0500

    Calculator version 2. (TWONRUNMAN)

---
+++ calculator/calculator.py
@@ -0,0 +1,43 @@
+def add(fno,lno):
+        return int(fno) + int(lno)
+
+def subtract(fno,lno):
+        return int(fno) - int(lno)
+
+def multiply(fno,lno):
+        return int(fno) * int(lno)
+
+def divide(fno,lno):
+        return int(fno) / int(lno)
+
+def operator(fno,lno,op):
+        if op == '+': return add(fno,lno)
+        elif op == '-': return subtract(fno,lno)
+        elif op == '*': return multiply(fno,lno)
+        elif op == '/': return divide(fno,lno)
+        else: print 'Invalid operator.\n'
+        return None
+
+def input1():
+	return raw_input("Enter 1st number : ")
+
+def input2():
+        return raw_input("Enter 2nd number : ")
+
+def inputope():
+        return raw_input("Enter operation : ")
+
+def output(n1,n2,ope,ans):
+	return ('{} {} {} = {}').format(n1,ope,n2,ans)
+
+def main():
+        n1 = input1()
+	n2 = input2()
+	ope = inputope()
+	ans = operator(n1,n2,ope)
+	print output(n1,n2,ope,ans)
+
+
+if __name__ == '__main__':
+  main()
+
